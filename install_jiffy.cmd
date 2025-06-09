e0b315c4f69a7504129f727437bdbc0e66f95912
Mofasi <49213017+Mofasi@users.noreply.github.com>
2025-06-10 00:27:21 +0200

    Update install_jiffy.cmd

--- install_jiffy.cmd
+++ install_jiffy.cmd
@@ -5,15 +5,15 @@ set JIFFY_DIR=C:\JIFFY
 :: Create JIFFY directory
 mkdir %JIFFY_DIR%
 
-:: Download latest jiify.php (replace URL with actual hosted version)
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yourserver.com/jiify.php', '%JIFFY_DIR%\jiify.php')"
+:: Download latest jiffy.php (replace URL with actual hosted version)
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yourserver.com/jiffy.php', '%JIFFY_DIR%\jiffy.php')"
 
 :: Add JIFFY to system PATH
 setx PATH "%PATH%;%JIFFY_DIR%"
 
 :: Create version check command
-echo @echo off > %JIFFY_DIR%\jiify_version.cmd
-echo echo JIFFY CLI Version 1.0 > %JIFFY_DIR%\jiify_version.cmd
+echo @echo off > %JIFFY_DIR%\jiffy_version.cmd
+echo echo JIFFY CLI Version 1.0 > %JIFFY_DIR%\jiffy_version.cmd
 
-echo Installation complete! Try running `jiify -v` to verify.
+echo Installation complete! Try running `jiffy -v` to verify.
 exit
