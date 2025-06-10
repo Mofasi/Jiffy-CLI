77087030ec84431c5e684a45b48d473037040b21
Mofasi <49213017+Mofasi@users.noreply.github.com>
2025-06-10 19:30:29 +0200

    Update uninstall_jiffy.cmd

--- uninstall_jiffy.cmd
+++ uninstall_jiffy.cmd
@@ -1,10 +1,10 @@
 @echo off
-set JIFFY_DIR=C:\JIFFY
+set "JIFFY_DIR=C:\Jiffy-CLI"
 
 :: Check if JIFFY is installed
-if not exist %JIFFY_DIR% (
+if not exist "%JIFFY_DIR%" (
     echo JIFFY CLI is not installed.
-    echo To install, run `install_jiffy.cmd`.
+    echo To install, run install_jiffy.cmd.
     echo.
     echo Press any key to continue...
     pause >nul
@@ -14,14 +14,11 @@ if not exist %JIFFY_DIR% (
 :: Prompt user for confirmation
 echo JIFFY is installed. Do you want to uninstall it? (Y/N)
 set /p CONFIRM_UNINSTALL=
-
 if /I "%CONFIRM_UNINSTALL%"=="Y" (
     echo Uninstalling JIFFY CLI...
-    rmdir /s /q %JIFFY_DIR%
-
-    :: Remove JIFFY from system PATH
-    powershell -Command "[System.Environment]::SetEnvironmentVariable('Path', ($env:Path -replace ';%JIFFY_DIR%'), 'Machine')"
-
+    rmdir /s /q "%JIFFY_DIR%"
+    :: Remove JIFFY_DIR from system PATH
+    powershell -noprofile -command "[Environment]::SetEnvironmentVariable('Path', ($env:Path -replace ';%JIFFY_DIR%',''), 'Machine')"
     echo ==============================================
     echo ✅ JIFFY CLI has been successfully removed!
     echo ==============================================
@@ -31,7 +28,6 @@ if /I "%CONFIRM_UNINSTALL%"=="Y" (
     echo ❌ Uninstallation canceled. JIFFY CLI remains installed.
     echo ==============================================
 )
-
 echo.
 echo Press any key to continue...
 pause >nul
